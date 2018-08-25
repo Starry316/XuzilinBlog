f99d6965cf527d80d01f51c87eea5e78752de07a
starry <18340018269@163.com>
2018-08-25 11:15:19 +0800

    增加用户简介

--- src/main/java/cn/xuzilin/blog/controller/UserController.java
+++ src/main/java/cn/xuzilin/blog/controller/UserController.java
@@ -5,10 +5,8 @@ import cn.xuzilin.blog.service.UserService;
 import cn.xuzilin.blog.util.ResponseUtil;
 import cn.xuzilin.blog.util.SessionUtil;
 import cn.xuzilin.blog.vo.ResponseVo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -72,6 +70,27 @@ public class UserController {
         UserPo userPo = SessionUtil.get(request,"userToken",UserPo.class);
         if (userPo == null)
             return ResponseUtil.error("未登录");
-        return ResponseUtil.success("success",userPo.getUser_name());
+        JSONObject respData = userService.getUserInfo(userPo.getUser_id());
+        respData.put("userName",userPo.getUser_name());
+        return ResponseUtil.success("success",respData);
+    }
+
+    /**
+     * 获取用户的详细信息
+     * @param request
+     * @return
+     */
+    @GetMapping("/getLoginUserInfo")
+    public ResponseVo getLoginUserInfo(HttpServletRequest request){
+        UserPo userPo = SessionUtil.get(request,"userToken",UserPo.class);
+        if (userPo == null)
+            return ResponseUtil.error("未登录");
+        JSONObject respData = userService.getUserInfo(userPo.getUser_id());
+        return ResponseUtil.success("success",respData);
+    }
+    @GetMapping("/getUserInfo/{userId}")
+    public ResponseVo getUserInfo(HttpServletRequest request, @PathVariable("userId") long userId){
+        JSONObject respData = userService.getUserInfo(userId);
+        return ResponseUtil.success("success",respData);
     }
 }
--- src/main/java/cn/xuzilin/blog/service/UserService.java
+++ src/main/java/cn/xuzilin/blog/service/UserService.java
@@ -1,8 +1,13 @@
 package cn.xuzilin.blog.service;
 
+import cn.xuzilin.blog.dao.UserInfoPoMapper;
 import cn.xuzilin.blog.dao.UserPoMapper;
+import cn.xuzilin.blog.po.UserInfoPo;
 import cn.xuzilin.blog.po.UserPo;
+import cn.xuzilin.blog.util.DateUtil;
 import cn.xuzilin.blog.util.PasswordUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -11,6 +16,9 @@ import javax.annotation.Resource;
 public class UserService {
     @Resource
     private UserPoMapper userPoMapper;
+
+    @Resource
+    private UserInfoPoMapper userInfoPoMapper;
 
     /**
      * 登录
@@ -36,6 +44,11 @@ public class UserService {
         userPo.setUser_name(userName);
         userPo.setUser_password(PasswordUtil.createHash(pass));
         userPoMapper.insertSelective(userPo);
+        UserInfoPo userInfoPo = new UserInfoPo();
+        userInfoPo.setUser_Id(userPo.getUser_id());
+        userInfoPo.setSigntime(DateUtil.getNowDate());
+        userInfoPo.setUser_describe("ta还没写介绍呢");
+        userInfoPoMapper.insertSelective(userInfoPo);
         return true;
     }
 
@@ -48,6 +61,16 @@ public class UserService {
         UserPo userPo = userPoMapper.selectByUserName(userName);
         if (userPo == null) return true;
         return false;
+    }
+
+    /**
+     * 获取用户信息
+     * @param userId
+     * @return
+     */
+    public JSONObject getUserInfo(long userId){
+        JSONObject data = JSON.parseObject(JSON.toJSONString(userInfoPoMapper.selectByPrimaryKey(userId)));
+        return data;
     }
 
     /**
